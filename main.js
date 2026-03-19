23be3da803d3015e4ce3829660e0cbc94b8fb725
ka1sser <serenokaisser.dev@gmail.com>
2026-03-19 23:00:16 +0800

    Done with the basic flow

--- main.js
+++ main.js
@@ -1,2 +1,55 @@
 import * as THREE from 'three';
 
+// 1. Create the scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// 2. Add the camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000);
+camera.position.z = 5;
+
+// 3. Create and add a cube object
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshLambertMaterial({
+    color: '#468585',
+    emissive: '#468585'
+})
+
+const sphere_geometry = new THREE.SphereGeometry()
+const sphere_material = new THREE.MeshBasicMaterial({
+    color: "#05ff16",
+    reflectivity: 1,
+    wireframe: true,
+});
+
+const cube = new THREE.Mesh(geometry, material);
+const sphere = new THREE.Mesh(sphere_geometry, sphere_material)
+sphere.position.set(1.5, 1.5, 0);
+scene.add(cube);
+scene.add(sphere);
+
+// 4. Add Lighting
+const light = new THREE.DirectionalLight(0x9CDBA6, 10);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Set up the renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// 6. Animate the scene
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    sphere.rotation.x += 0.005;
+    sphere.rotation.y += 0.005;
+    renderer.render(scene, camera);
+}
+
+animate();
